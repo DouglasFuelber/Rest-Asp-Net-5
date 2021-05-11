324d40a5aae56a89e0ae6c00c819289ea44f46f9
Douglas Fuelber <contato@douglasfuelber.com>
2021-05-10 22:23:36 -0300

    Started HATEOAS implementation

---
+++ RestAspNet5/Hypermedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestAspNet5.Hypermedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
---
+++ RestAspNet5/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using RestAspNet5.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestAspNet5.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
